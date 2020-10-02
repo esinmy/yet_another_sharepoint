b35c9e84d5eef211cad5ae39121f8147845fcfe5
Maxim Esin <esinmy@gmail.com>
2020-10-02 23:51:55 +0300

    Added DbContext, cntx string, configureservices  and migration

---
+++ PortalBackend/Migrations/20201002204751_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortalBackend.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FamilyName = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    PatronymName = table.Column<string>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
--- PortalBackend/Models/Person.cs
+++ PortalBackend/Models/Person.cs
@@ -1,13 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PortalBackend.Models
 {
     public class Person
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string FamilyName { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string PatronymName { get; set; }
+        [Required]
         public DateTime Birthday { get; set; }
     }
 }
